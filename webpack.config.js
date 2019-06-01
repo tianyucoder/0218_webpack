f7268dc93adbd1604048244248f84fa289448ec5
peiqi <peiqi@sina.com>
2019-06-01 11:55:20 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,10 @@
  *  问题：这种方式只能够编译打包js、json文件，其他文件处理不了
  * 5.webpack --display-modules可以查看隐藏的任务
  */
-const {resolve} = require('path'); //path内置的模块，用来设置路径。
+//path内置的模块，用来设置路径。
+const {resolve} = require('path');
+//引入extract-text-webpack-plugin提取css为单独文件
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   //入口（从哪里进入开始解析）
@@ -28,7 +31,7 @@ module.exports = {
   module: {
     rules: [
       //1.less-loader将less转换为css
-      {
+      /*{
         test: /\.less$/,
         use: [{
           loader: 'style-loader' //通过js动态向页面中构建一个style标签，并且将css样式注入
@@ -37,8 +40,51 @@ module.exports = {
         }, {
           loader: 'less-loader' //将less编译为css
         }]
+      },*/
+      {
+        test: /\.less$/, //匹配文件的规则，说明该loader对哪个文件生效
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader","less-loader"]
+        })
+      },
+      //2.file-loader处理图片资源
+      /*{
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath:'img',//图片最终输出的位置
+              publicPath:'../build/img',//css资源图片路径
+              name:'[hash:5].[ext]'  //修改图片名称
+            },
+          },
+        ],
+      },*/
+      //3.使用url-loader使图片转base64
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              outputPath:'img',//图片最终输出的位置
+              publicPath:'../img',//css资源图片路径
+              name:'[hash:5].[ext]',  //修改图片名称
+              limit: 8192 //当图片小于8k的时，转base64
+            }
+          }
+        ]
       }
     ]
-  }
+  },
+
+  //配置所有的plugins
+  plugins: [
+    //提取css为单独文件
+   new ExtractTextPlugin("./css/index.css"),
+  ]
+
 };
 
